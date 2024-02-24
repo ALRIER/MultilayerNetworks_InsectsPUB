f23b02b0530c57090cd8d2e2a2ab30ffc0e8e57c
Juan C. Correa <j.correa.n@gmail.com>
2024-02-24 15:00:02 -0600

    Update EdibleInsectsCleanedData.R

--- EdibleInsectsCleanedData.R
+++ EdibleInsectsCleanedData.R
@@ -11,4 +11,6 @@ EI <- list(EI1, EI2, EI3, EI4, EI5, EI6, EI7)
 EI <- do.call("rbind", EI)
 rm(list=setdiff(ls(), "EI"))
 
+
 EI <- subset(EI, EI$Genus!='Genus')
+save.image("~/Documents/GitHub/MultilayerNetworks_InsectsPUB/Data/EdibleInsectsList.RData")

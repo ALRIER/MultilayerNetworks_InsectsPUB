1cf3e5e32240d1fd2c677bff9427db9a0171f859
Juan C. Correa <j.correa.n@gmail.com>
2024-02-25 00:52:02 -0600

    Update BipartiteNetworkInsectsCountries.R

--- Study_1/BipartiteNetworkInsectsCountries.R
+++ Study_1/BipartiteNetworkInsectsCountries.R
@@ -2,11 +2,20 @@ library(readr)
 EdibleSpecies <- read_delim("Study_1/EdibleSpecies.csv", 
                             delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
-
+load("Study_1/Study1A.RData")
+Edibles <- EdibleSpecies %>% mutate(., Importance = ifelse(grepl("Moth", CommonName), Centralities$Eigen[10],
+                                                    ifelse(grepl("Larvae", CommonName), Centralities$Eigen[1],
+                                                    ifelse(grepl("Locust", CommonName), Centralities$Eigen[11],
+                                                    ifelse(grepl("Beetle", CommonName), Centralities$Eigen[2],
+                                                    ifelse(grepl("Cricket", CommonName), Centralities$Eigen[12],
+                                                    ifelse(grepl("Ants", CommonName), Centralities$Eigen[4],
+                                                    ifelse(grepl("Termites", CommonName), Centralities$Eigen[8],
+                                                    ifelse(grepl("Flies", CommonName), Centralities$Eigen[5],
+                                                                  median(Centralities$Eigen))))))))))
 
 library(ggplot2)
-ggplot(EdibleSpecies, aes(x=Energy, y=Protein, color=CommonName)) +
-  geom_point() + theme_bw()
+ggplot(Edibles, aes(x=Energy, y=Protein, color=CommonName)) +
+  geom_point(aes(size = Importance), alpha = 0.7) + theme_minimal()
 
 
 
